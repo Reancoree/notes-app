406110ff7a03894dfdb92a40ec5dd330404af8c2
Reancoree <reancoree@gmail.com>
2025-01-30 20:59:24 +0300

    Note created

--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'django-insecure--kny91p=yo68y81vu$uts^om^(cctfyhg!jxs36&z7q^vr6fh@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'note',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'UTC'
 
