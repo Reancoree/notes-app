e29a7f273d76b3258475a37a4de81d7e08ddf3c1
Reancoree <reancoree@gmail.com>
2025-02-09 18:34:41 +0300

    Добавил профиль, логин и выход

--- todo_app/note/urls.py
+++ todo_app/note/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('delete_note/<int:pk>/', views.DeleteNotePage.as_view(), name='delete_note'),
     path('category/', views.CategoryPage.as_view(), name='category'),
     path('category/add/', views.AddCategoryPage.as_view(), name='add_category'),
-    path('category/<slug:slug>/', views.UpdateCategoryPage.as_view(), name='category'),
+    path('category/<slug:slug>/', views.UpdateCategoryPage.as_view(), name='category_slug'),
     path('category/delete/<slug:slug>/', views.DeleteCategoryPage.as_view(), name='delete_category'),
 ]
--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from django.urls import reverse_lazy
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -30,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'note',
+    'user',
     'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -119,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = reverse_lazy('user:profile')
