cbeddbf829b9618dca47f40b4db5c0d21e7e0de8
Reancoree <reancoree@gmail.com>
2025-02-09 18:58:19 +0300

    Сделал регистрацию

---
+++ todo_app/user/forms.py
@@ -0,0 +1,7 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, BaseUserCreationForm
+
+
+class RegisterForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = get_user_model()
--- todo_app/user/urls.py
+++ todo_app/user/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+
 from user import views
 
 app_name = 'user'
 
 urlpatterns = [
-    path('profile/', views.profile_view, name='profile')
+    path('profile/', views.profile_view, name='profile'),
+    path('accounts/regiser/', views.RegisterView.as_view(), name='register')
 ]
